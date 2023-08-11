7ff1cd30213e375942fa71a7b94cb53c893bda47
sebastien.durocher <the_living_nazghul@hotmail.com>
2023-08-11 17:15:25 -0400

    Forgot to update version (0.1 -> 0.2) by adding Get_Daymet_Area.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='GetWeatherData',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=["xarray >= 2023.7.0","xclim >= 0.44.0", "siphon >= 0.9", "pystac", "fsspec", "clisops", "zarr"],
     python_requires=">=3.9.0",

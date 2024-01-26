c59da9fb25af43491ae34c5a5a4f422220dd66d2
sebastien.durocher <the_living_nazghul@hotmail.com>
2024-01-26 14:51:50 -0500

    First commit

--- source/__init__.py
+++ source/__init__.py
@@ -10,7 +10,7 @@ Notes:
 """
 
 from .Observations.NRCAN import get_nrcan
-from .Observations.Daymet import Get_Daymet_Area
+from .Observations.Daymet import Get_Daymet_Area_Old
 from .Reanalyses.ERA5 import era5_from_pavics
 from .Reanalyses.RDRS import rdrs_from_pavics
 

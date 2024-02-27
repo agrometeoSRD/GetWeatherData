61e801bf067366bb1ceaf94898da66fddcfa394e
sebastien.durocher <the_living_nazghul@hotmail.com>
2024-02-27 16:24:29 -0500

    added a line for config.json required for loading SM station data.

--- setup.py
+++ setup.py
@@ -13,8 +13,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='GetWeatherData',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=["xarray >= 2023.7.0","xclim >= 0.44.0", "siphon >= 0.9", "pystac", "fsspec", "clisops", "zarr"],
     python_requires=">=3.9.0",
+    package_data={
+        "":["*.json"]
+    }
 )

eb6728b402d0c5adf5ef16cc1ff824913ef76a23
sebastien.durocher <the_living_nazghul@hotmail.com>
2024-03-18 13:34:51 -0400

    ...

---
+++ run/Load_EnvCan_Forecast_p1.bat
@@ -0,0 +1,5 @@
+cd C:\scripts\GetWeatherData
+
+python -m source.Forecasts.ec_forecasts --dat-file {ReplaceHereWithProjectPath}\GetWeatherData\source\Forecasts\VStations.dat
+
+python -m source.Forecasts.save_ec_nowcast

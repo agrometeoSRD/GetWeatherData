e58a408ff0360c1b6a4ee3fb946708adc56bc39f
sebastien.durocher <the_living_nazghul@hotmail.com>
2024-04-03 11:31:04 -0400

    Deleted scripts

--- run/Load_EnvCan_Forecast_p1.bat
+++
@@ -1,5 +0,0 @@
-cd C:\scripts\GetWeatherData
-
-python -m source.Forecasts.ec_forecasts --dat-file {ReplaceHereWithProjectPath}\GetWeatherData\source\Forecasts\VStations.dat
-
-python -m source.Forecasts.save_ec_nowcast

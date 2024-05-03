5625017530fff216eec5b66a40b08f0d97429ccf
sebastien.durocher <the_living_nazghul@hotmail.com>
2024-05-02 21:00:55 -0400

    First commit.

---
+++ process/combine_wu_hrdps.py
@@ -0,0 +1,19 @@
+"""
+File: combine_wu_hrdps.py
+Author: sebastien.durocher
+Email: sebastien.rougerie-durocher@irda.qc.ca
+Github: https://github.com/MorningGlory747
+Description: This is a description of what the script does
+Created: 2024-05-02
+"""
+
+# Import statements
+
+# Constants
+
+# Functions
+
+# Main execution ---------------------------------------
+
+if __name__ == "__main__":
+    pass
